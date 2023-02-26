dc35585c841d9ef13b5df648e4e9b251bdaa66bb
Rhythm Bellic <92723976+Rhythmbellic@users.noreply.github.com>
2023-02-26 14:14:58 +0530

    Update recom.py

--- recom.py
+++ recom.py
@@ -38,13 +38,49 @@ def converterpro(obj):
             l.append(i['name'])
             a=a+1
     return l
-  data1['cast']=data1['cast'].apply(converterpro)
-  def converter2(obj):
-    l=[]
-    for i in ast.literal_eval(obj):
-        if i['job']=='Director':
-            l.append(i['name'])
-            break
-    return l
-  data1['crew']=data1['crew'].apply(converter2)
-  data1['overview']=data1['overview'].apply(lambda x:x.split())
+data1['cast']=data1['cast'].apply(converterpro)
+def converter2(obj):
+  l=[]
+  for i in ast.literal_eval(obj):
+      if i['job']=='Director':
+          l.append(i['name'])
+          break
+  return l
+data1['crew']=data1['crew'].apply(converter2)
+data1['overview']=data1['overview'].apply(lambda x:x.split())
+data1['genres']=data1['genres'].apply(lambda x:[i.replace(' ','')for i in x])
+data1['keywords']=data1['keywords'].apply(lambda x:[i.replace(' ','')for i in x])
+data1['cast']=data1['cast'].apply(lambda x:[i.replace(' ','')for i in x])
+data1['crew']=data1['crew'].apply(lambda x:[i.replace(' ','')for i in x])
+data1['production_companies']=data1['production_companies'].apply(lambda x:[i.replace(' ','')for i in x])
+data1['tags']=data1['overview']+data1['genres']+data1['keywords']+data1['cast']+data1['crew']+data1['production_companies']
+moviedata=data1[['movie_id','title','tags']]
+moviedata['tags']=moviedata['tags'].apply(lambda x:' '.join(x))
+moviedata['tags']=moviedata['tags'].apply(lambda x:x.lower())
+#our data preproessing is done now we will work on modelling
+#now we will do textvectorization using bag of words technique
+import nltk
+from nltk.stem.porter import PorterStemmer
+ps=PorterStemmer()
+def stem(text):
+    y=[]
+    for i in text.split():
+        y.append(ps.stem(i))
+    return ' '.join(y)
+moviedata['tags']=moviedata['tags'].apply(stem)
+from sklearn.feature_extraction.text import CountVectorizer
+cv=CountVectorizer(max_features=5000,stop_words='english')
+vec=cv.fit_transform(moviedata['tags']).toarray()
+from sklearn.metrics.pairwise import cosine_similarity
+similarity=cosine_similarity(vec)
+def recommend(movie):
+    movie_index=moviedata[moviedata['title']==movie].index[0]
+    distances=similarity[movie_index]
+    movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+    for i in movies_list:
+        print(moviedata.iloc[i[0]].title)
+a=input("enter the name of movie")
+b=recommend(a)
+print(b)
+
+
